94fe2b87ca9f3dfdb1546d8302241c7332747ad4
Pete Richardson <pete@peterichardson.com>
2024-06-09 13:28:51 -0700

    wc-rs - add counting from stdin (specify - as file pattern)
    Works with and without other patterns

--- wc-rs/src/main.rs
+++ wc-rs/src/main.rs
@@ -3,7 +3,7 @@ use glob::glob;
 use log::debug;
 use std::fs::File;
 use std::io::BufRead;
-use std::path::Path;
+use std::io::{stdin, BufReader, Read};
 use threadpool::ThreadPool;
 
 use memmap2::Mmap;
@@ -14,16 +14,24 @@ struct Args {
     filename_patterns: Vec<String>,
 }
 
-/// count lines in file by:
-/// 1. memory mapping file
-/// 2. calling lines() on the whole slice
-/// 3. iterating and accumulating the count
-fn count_lines(path: &Path) -> Result<usize, Box<dyn std::error::Error>> {
+fn count_lines_from_bytes(bytes: &[u8]) -> usize {
+    bytes
+        .lines()
+        .try_fold(0, |acc: usize, line| line.map(|_| acc + 1))
+        .expect("couldn't fold")
+}
+
+fn count_stdin() -> Result<usize, std::io::Error> {
+    let mut input: String = Default::default();
+    let stdin = stdin().lock();
+    let _ = BufReader::new(stdin).read_to_string(&mut input)?;
+    Ok(count_lines_from_bytes(input.as_bytes()))
+}
+
+fn count_file(path: &str) -> Result<usize, Box<dyn std::error::Error>> {
     let file = File::open(path)?;
     let mmap = unsafe { Mmap::map(&file)? };
-    let mut lines = mmap[..].lines();
-    let count = lines.try_fold(0, |acc, line| line.map(|_| acc + 1))?;
-    Ok(count)
+    Ok(count_lines_from_bytes(&mmap[..]))
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -34,11 +42,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for pattern in args.filename_patterns {
         debug!("# handling pattern '{}'", &pattern);
+        if pattern == "-" {
+            let count: usize = count_stdin().expect("problem reading stdin");
+            println!("{:8} stdin", count);
+            continue;
+        }
         for entry in glob(&pattern)? {
             debug!("# handling entry '{:?}'", &entry);
             match entry {
                 Ok(path) => pool.execute(move || {
-                    let count: usize = count_lines(&path).expect("Couldn't count lines!");
+                    let count: usize =
+                        count_file(&path.to_string_lossy()).expect("Couldn't count lines!");
                     println!("{:8} {}", count, path.display());
                 }),
                 Err(e) => println!("{:?}", e),
@@ -56,9 +70,6 @@ mod tests {
 
     #[test]
     fn test_count_lines() {
-        assert_eq!(
-            count_lines("data/AliceInWonderland.txt".as_ref()).unwrap(),
-            24
-        );
+        assert_eq!(count_file("data/AliceInWonderland.txt").unwrap(), 24);
     }
 }
