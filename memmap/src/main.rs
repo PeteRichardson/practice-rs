91c1f364ce707bb9d2a9e5d406f1058a3f6fc977
Pete Richardson <pete@peterichardson.com>
2024-06-08 18:00:35 -0700

    memmap - count lines of files matching glob patterns on the cmd line
    basically wc -l using memory mapped files

--- memmap/src/main.rs
+++ memmap/src/main.rs
@@ -1,13 +1,39 @@
+use clap::Parser;
+use glob::glob;
+use log::debug;
 use std::fs::File;
-// use std::io::Read;
+use std::io::BufRead;
 
 use memmap2::Mmap;
 
-fn main() -> Result<(), std::io::Error> {
-    let file = File::open("/usr/share/dict/words")?;
+#[derive(Parser, Debug)]
+struct Args {
+    filename_patterns: Vec<String>,
+}
 
-    let mmap = unsafe { Mmap::map(&file)? };
-    println!("{}", &mmap[..].len());
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let args = Args::parse();
+
+    for pattern in args.filename_patterns {
+        debug!("# handling pattern '{}'", &pattern);
+        for entry in glob(&pattern)? {
+            debug!("# handling entry '{:?}'", &entry);
+            match entry {
+                Ok(path) => {
+                    let file = File::open(&path)?;
+
+                    let mmap = unsafe { Mmap::map(&file)? };
+                    let mut lines = mmap[..].lines();
+                    let count = lines.try_fold(0, |acc, line| line.map(|_| acc + 1))?;
+
+                    println!("{:8}  {}", count, &path.display());
+                }
+                Err(e) => println!("{:?}", e),
+            }
+        }
+    }
 
     Ok(())
 }
