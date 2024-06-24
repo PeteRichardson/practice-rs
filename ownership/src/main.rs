b789289cecf5b32f2c68b4de3e3e2c7665c127a3
Pete Richardson <pete@peterichardson.com>
2024-06-23 17:42:06 -0700

    break out append to function

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,17 @@
+// Running through some examples from chapter 4 of
+// The Rust Programming Language, 2nd ed.
+
+fn append_s(s: &mut str) -> String {
+    s.to_owned()  + "s"
+}
+
 fn main() {
-    let mut s = String::from("Hello");
-    s.push_str(", World!");
-    println!("{}", s);
+    let s0 : String;
+    {
+        let mut s1 = String::from("Hello");
+        s1 = append_s(&mut s1);
+        s0 = s1;
+        //println!("s1: {}", s1);
+    }
+    println!("s0: {}", s0);
 }
