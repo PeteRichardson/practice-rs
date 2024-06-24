1d5cbff5ffafbb414037e38a8ce866e41666bbec
Pete Richardson <pete@peterichardson.com>
2024-06-23 21:47:40 -0700

    ownership - simpler smaller code

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,12 @@
 // Running through some examples from chapter 4 of
 // The Rust Programming Language, 2nd ed.
 
-fn append_s(s: &mut str) -> String {
-    s.to_owned()  + "s"
+fn append_s(s: &mut String) { 
+    s.push('s')
 }
 
 fn main() {
-    let s0 : String;
-    {
-        let mut s1 = String::from("Hello");
-        s1 = append_s(&mut s1);
-        s0 = s1;
-        //println!("s1: {}", s1);
-    }
+    let mut s0 = String::from("Hello"); // s0's scope begins. It's a mutable string
+    append_s(&mut s0);
     println!("s0: {}", s0);
 }
