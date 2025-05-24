841ae01e2c34cdbf6bbce66bf32590b9750dffb3
Pete Richardson <pete@peterichardson.com>
2025-05-24 12:33:23 -0700

    feat: added code to walk the dir.
    Added anyhow crate to simplify error handling

--- async1/src/main.rs
+++ async1/src/main.rs
@@ -1,6 +1,9 @@
+use anyhow::Context;
+use async_std::fs;
+use async_std::path::PathBuf;
+use async_std::prelude::*;
 use async_std::task;
 use clap::Parser;
-use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Args {
@@ -9,17 +12,33 @@ struct Args {
     dir: PathBuf,
 }
 
-async fn walk_dir(dir: PathBuf) {
-    println!("Hello, {}!", dir.display());
+async fn walk_dir(dir: PathBuf) -> anyhow::Result<()> {
+    let mut entries = fs::read_dir(&dir)
+        .await
+        .with_context(|| format!("Failed to read directory: {}", dir.display()))?;
+
+    while let Some(entry_res) = entries
+        .next()
+        .await
+        .transpose()
+        .with_context(|| format!("Failed to read entry in {}", dir.display()))?
+    {
+        let path = entry_res.path();
+        println!("# Checking dir: {}", path.display());
+    }
+
+    Ok(())
 }
 
 #[async_std::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let root_dir = args.dir;
 
     let handle = task::spawn(async {
-        walk_dir(root_dir).await;
+        let _ = walk_dir(root_dir).await;
     });
     handle.await;
+
+    Ok(())
 }
