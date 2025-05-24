2fa6101b18b23d55abbd0f7e4970a51305b410d6
Pete Richardson <pete@peterichardson.com>
2025-05-24 11:53:52 -0700

    feat: add -d option for starting (root) directory.   Uses clap crate

--- async1/src/main.rs
+++ async1/src/main.rs
@@ -1,9 +1,25 @@
 use async_std::task;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    // Starting Directory (default ".")
+    #[arg(short = 'd', long = "dir", default_value = ".")]
+    dir: PathBuf,
+}
+
+async fn walk_dir(dir: PathBuf) {
+    println!("Hello, {}!", dir.display());
+}
 
 #[async_std::main]
 async fn main() {
+    let args = Args::parse();
+    let root_dir = args.dir;
+
     let handle = task::spawn(async {
-        println!("Hello, world!");
+        walk_dir(root_dir).await;
     });
     handle.await;
 }
